22fea62cbf35667026328efa24ce8600a4c21025
Angel <aavila002@citymail.cuny.edu>
2023-01-14 15:00:07 -0500

    Challenge 6 Commits for Pre-work

--- index.js
+++ index.js
@@ -170,12 +170,24 @@ allBtn.addEventListener("click", showAllGames);
 const descriptionContainer = document.getElementById("description-container");
 
 // use filter or reduce to count the number of unfunded games
+const numUnfunded = GAMES_JSON.reduce((acc, game) => {
+    if(game.goal > game.pledged){
+        return acc + 1;
+    }else{
+        return acc;
+    }
+}, 0)
 
 
 // create a string that explains the number of unfunded games using the ternary operator
-
+const displayStr = `A total of $${totalRaised.toLocaleString("en-US")} 
+has been raised for ${GAMES_JSON.length} games. 
+Currently, ${numUnfunded} ${numUnfunded == 1 ? `game remains`:`games remain`} unfunded. 
+We need your help to fund these amazing games!`
 
 // create a new DOM element containing the template string and append it to the description container
+let challenge6Text = `<p>${displayStr}</p>`;
+descriptionContainer.innerHTML += challenge6Text
 
 /************************************************************************************
  * Challenge 7: Select & display the top 2 games
